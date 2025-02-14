54e5f60fb27f8cfd21f5af4f5ef27c6e539e2036
Leon <160693275+Leon9724@users.noreply.github.com>
2025-02-13 17:05:40 -0800

    Add files via upload

---
+++ homework2/hw2.py
@@ -0,0 +1,38 @@
+# 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+cd ~/Desktop/
+# 2) What does ~/ mean?
+it mean my home directory 
+# 3) What's the difference between and abolute and a relative path in your terminal?
+absolute path is the full path of a file. eg. cd /c/Users/username/Desktop
+relative path is a shorter and faster way to find a file. eg. cd homework2
+# 4) What command would bring you back to your home directory?
+cd ~
+# 5) If you called rm ./ in your current working directory, what would it do? 
+it deletes all files in the current directory.
+
+# 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+git add moves files from current directory to staging area.
+git commit saves changes to local repository
+git push pushes changes from local to remote repository
+
+# 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+$ git status
+On branch master
+Changes to be committed:
+  (use "git restore --staged <file>..." to unstage)
+        new file:   hello.txt
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        .vscode/
+        homework2/hw2.py
+It tells me the txt file is in staging area.
+
+# 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+when typing in vscode, if I wanna change or insert a text in the middle of a phrase, the cursor would delete the next letter and I have to redo everything after that.
+Solved by turning off overtype
+# 9) How does cloning a repository differ from pulling from a repository?
+cloning is copying a remote repo to my pc, but pulling is downloading changes from remote repo to local. 
+
+# 10) Tell me a fun fact!
+I used to learn a little bit of python, but I forgot most of it.
